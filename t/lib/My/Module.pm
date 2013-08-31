436c4b82f402aa8a60e549970780c3ed653c4910
Jakob Voss <voss@gbv.de>
2013-08-31 20:55:29 +0200

    added Log::Contextual::Easy for zero-config usage

---
+++ t/lib/My/Module.pm
@@ -0,0 +1,13 @@
+package My::Module;
+use Log::Contextual::Easy;
+
+sub log {
+    Dlog_fatal { $_ }
+    DlogS_error { $_ }
+    logS_warn  { $_[0] }
+    logS_info  { $_[0] }
+    log_debug { $_[0] }
+    log_trace { $_[0] } 'xxx';
+}
+
+1;
